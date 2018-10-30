e28996fd060caecd81ab188826376d5c71da3fda
Anuj Panchal <anujpan1@gmail.com>
2018-10-30 17:19:38 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,8 +15,6 @@ class Patient
 end
 
 
-# A patient is instantiated with a name and be saved in the @@all array.
-
 # The Patient class needs an instance method, #new_appointment, that takes in an argument of a doctor and a date and creates a new appointment. The appointment should know that it belongs to the patient.
 
 # The Patient class needs an instance method, #appointments, that iterates through the appointments array and returns appointments that belong to the patient.
