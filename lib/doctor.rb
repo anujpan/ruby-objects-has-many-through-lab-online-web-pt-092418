f0b160f5304cac6f56a0943c318d4ee9e5f9a5a2
Anuj Panchal <anujpan1@gmail.com>
2018-10-30 17:25:39 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
   end
   
   def new_appointment(date, patient)
-    appointment = Appointment.new(patient, date, self)
+    appointment = Appointment.new(date, patient, self)
   end
 
   def appointments
