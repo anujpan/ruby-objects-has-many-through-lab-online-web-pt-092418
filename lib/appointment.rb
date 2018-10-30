a61e971ecdea1d218a2817e0aa06a431763697b4
Anuj Panchal <anujpan1@gmail.com>
2018-10-30 17:59:43 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -15,8 +15,3 @@ class Appointment
     @@all
   end
 end
-
-
-# The Appointment class needs a class method .all that lists each appointment in the class variable.
-
-# An appointment should be initialized with a date (as a string, like "Monday, August 1st"), a patient, and a doctor. The appointment should be saved in the @@all array.
